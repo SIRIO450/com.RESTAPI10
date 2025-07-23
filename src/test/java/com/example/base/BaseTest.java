cf10c70b4407698b254525ac58840dc6f3b570b3
SIRIO450 <rajeswarikommiri@gmail.com>
2025-07-23 16:02:12 +0530

    Create BaseTest.java

---
+++ src/test/java/com/example/base/BaseTest.java
@@ -0,0 +1,12 @@
+package com.example.base;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+}
